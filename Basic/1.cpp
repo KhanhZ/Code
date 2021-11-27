840ed31cb9fd1829bbca9ad950a4c0f4c493c61c
KhanhZ <tdkgaming2006@gmail.com>
2021-11-27 21:17:57 +0700

    Con ốc sên

--- Basic/1.cpp
+++ Basic/1.cpp
@@ -1,140 +1,20 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main(){
-    int a, b;
-    cin >> a >> b;
-    if (a % 10 == 0)
-    {
-        cout << 0;
+int main ()
+{
+    int a, b, v;
+    cin >> a >> b >> v;
+    if (v <= a) {
+        cout << '1';
     }
-    else if ((a - 1) % 10 == 0)
-    {
-        cout << 1;
-    }
-    else if ((a - 4) % 10 == 0)
-    {
-        if (b % 2 == 0)
-        {
-            cout << 6;
+    else {
+        if ((v - b) % (a - b) == 0 && b < a) {
+            cout << (v - b) / (a - b);
         }
-        else
-        {
-            cout << 4;
-        }
-    }
-    else if ((a - 5) % 10 == 0)
-    {
-        cout << 5;
-    }
-    else if ((a - 6) % 10 == 0)
-    {
-        cout << 6;
-    }
-    else if ((a + 1) % 10 == 0)
-    {
-        if (b % 2 == 0)
-        {
-            cout << 1;
-        }
-        else
-        {
-            cout << 9;
-        }
-    }
-    else if ((a - 2) % 10 == 0)
-    {
-        if (b % 4 == 1)
-        {
-            cout << 2;
-        }
-        else if (b % 4 == 2)
-        {
-            cout << 4;
-        }
-        else if (b % 4 == 3)
-        {
-            cout << 8;
-        }
-        else
-        {
-            cout << 6;
-        }
-    }
-    else if ((a - 3) % 10 == 0)
-    {
-        if (b % 4 == 1)
-        {
-            cout << 3;
-        }
-        else if (b % 4 == 2)
-        {
-            cout << 9;
-        }
-        else if (b % 4 == 3)
-        {
-            cout << 7;
-        }
-        else
-        {
-            cout << 1;
-        }
-    }
-    else if ((a - 3) % 10 == 0)
-    {
-        if (b % 4 == 1)
-        {
-            cout << 3;
-        }
-        else if (b % 4 == 2)
-        {
-            cout << 9;
-        }
-        else if (b % 4 == 3)
-        {
-            cout << 7;
-        }
-        else
-        {
-            cout << 1;
-        }
-    }
-    else if ((a - 7) % 10 == 0)
-    {
-        if (b % 4 == 1)
-        {
-            cout << 7;
-        }
-        else if (b % 4 == 2)
-        {
-            cout << 9;
-        }
-        else if (b % 4 == 3)
-        {
-            cout << 3;
-        }
-        else
-        {
-            cout << 1;
-        }
-    }
-    else if ((a + 2) % 10 == 0)
-    {
-        if (b % 4 == 1)
-        {
-            cout << 8;
-        }
-        else if (b % 4 == 2)
-        {
-            cout << 4;
-        }
-        else if (b % 4 == 3)
-        {
-            cout << 2;
-        }
-        else
-        {
-            cout << 6;
+        else {
+            cout << (v - b) / (a - b) + 1;
         }
     }
     return 0;
